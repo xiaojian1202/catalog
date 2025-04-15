11bc2706d8366ec979cf2a88a038056837009304
xiaojian1202 <chenx2003@gmail.com>
2025-04-15 06:21:40 -0700

    Updated to implement data into html and css

--- scripts.js
+++ scripts.js
@@ -30,12 +30,15 @@ const CURB_POSTER_URL =
 const EAST_LOS_HIGH_POSTER_URL =
   "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
+import { games } from "./data.js";
+
 // This is an array of strings (TV show titles)
-let titles = [
+/*let titles = [
   "Fresh Prince of Bel Air",
   "Curb Your Enthusiasm",
   "East Los High",
-];
+  "The Office",
+];*/
 // Your final submission should have much more data than this, and
 // you should use more than just an array of strings to store it all.
 
@@ -45,40 +48,107 @@ function showCards() {
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
+  for (let i = 0; i < games.length; i++) {
+    const curr = games[i];
+    /*let name = games[i].name;
+    let description = games[i].description;
+    let genre = games[i].genre;
+    let steam = games[i].ratings[0];
+    let imdb = games[i].ratings[1];
+    let metacritic = games[i].ratings[2];
+    let publisher = games[i].publisher;
+    let country = games[i].country;
+    let releaseDate = games[i].releaseDate;
+    let platforms = games[i].platforms;
+    let price = games[i].price;
+    let imgURL = games[i].imageURL;*/
     // This part of the code doesn't scale very well! After you add your
     // own data, you'll need to do something totally different here.
-    let imageURL = "";
+    /*let imageURL = "";
     if (i == 0) {
       imageURL = FRESH_PRINCE_URL;
     } else if (i == 1) {
       imageURL = CURB_POSTER_URL;
     } else if (i == 2) {
       imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
+    }*/
+      
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, curr); // Edit the contents of the card
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+function editCardContent(card, game) {
   card.style.display = "block";
 
+  //Updates the name of the card
   const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
+  cardHeader.textContent = game.name;
 
+  //Updates image of the card
   const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
+  cardImage.src = game.imageURL;
+  cardImage.alt = game.name + " Game Cover";
+
+  //Update genre of the card
+  const cardGenre = card.querySelector(".card-genres");
+  for (let i=0; i<game.genre.length; i++){
+    const genreName = document.createElement("span");
+    genreName.classList.add("genre-name");
+    genreName.textContent = game.genre[i];
+    cardGenre.appendChild(genreName);
+  }
+
+  //Update description of the card
+  const cardDescription = card.querySelector(".card-description");
+  if (cardDescription) {
+    cardDescription.textContent = game.description;
+  }
+
+  //Update price of the card
+  const cardPrice = card.querySelector(".price");
+  if (cardPrice) {
+    cardPrice.textContent = game.price;
+  }
+
+  //Update ratings of the card
+  const cardRatings = card.querySelector(".card-ratings");
+  const reviewText = document.createElement("span");
+  reviewText.classList.add("review-text");
+  reviewText.textContent =  game.ratingSteam[0];
+  cardRatings.appendChild(reviewText);
+
+  const reviewCount = document.createElement("span");
+  reviewCount.classList.add("review-count");
+  reviewCount.textContent = game.ratingSteam[1];
+  cardRatings.appendChild(reviewCount);
+
+  //Update publisher of the card
+  const cardPublisher = card.querySelector(".publisher");
+  if (cardPublisher) {
+    cardPublisher.textContent = "Publisher: " + game.publisher;
+  }
+
+  //Update country of the card
+  const cardDate = card.querySelector(".release-date");
+  if (cardDate) {
+    cardDate.textContent = "Release: " + game.releaseDate;
+  }
+
+  //Update platforms of the card
+  const cardPlatforms = card.querySelector(".platforms");
+  for (let i=0; i<game.platforms.length; i++){
+    const platform = document.createElement("span");
+    platform.classList.add("platform");
+    platform.textContent = game.platforms[i] + " ";
+    cardPlatforms.appendChild(platform);
+  }
 
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
   // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
+  console.log("new card:", game.name, "- html: ", card);
 }
 
 // This calls the addCards() function when the page is first loaded
@@ -92,6 +162,6 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
+  games.pop(); // Remove last item in titles array
   showCards(); // Call showCards again to refresh
 }
